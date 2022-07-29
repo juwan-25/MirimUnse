5cfde100facc5ad591a5534da648a96cb89a6290
지민 백 <qorwlalsjimin@naver.com>
2022-07-29 14:35:19 +0900

    Activity 생성

---
+++ app/src/main/java/kr/hs/s2104/mirimunse/LuckCheckDetailActivity.java
@@ -0,0 +1,14 @@
+package kr.hs.s2104.mirimunse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LuckCheckDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_luck_check_detail);
+    }
+}
