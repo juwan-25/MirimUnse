8cdbef4492734093dadc95cd1d55738b067b31a6
juwan-25 <s2109@e-mirim.hs.kr>
2022-08-02 13:46:29 +0900

    main에서 luckcheck으로 전환

--- app/src/main/java/kr/hs/s2104/mirimunse/MainActivity.java
+++ app/src/main/java/kr/hs/s2104/mirimunse/MainActivity.java
@@ -2,7 +2,11 @@ package kr.hs.s2104.mirimunse;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,11 +14,19 @@ public class MainActivity extends AppCompatActivity {
     //insertData: 데이터 삽입 메서드
     //viewData: 데이터 조회 메서드
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         db = new DatabaseHelper(this);
-
+        ImageView imgToChek = findViewById(R.id.img_to_check);
+        imgToChek.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), LuckCheckActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
