d241696c06a42ef32334a387e4e7aeed771fabf8
steam88ys <yssteam88@gmail.com>
2022-08-02 14:08:20 +0900

    activity_luck_record_detail.xml 연결

--- app/src/main/java/kr/hs/s2104/mirimunse/LuckRecordActivity.java
+++ app/src/main/java/kr/hs/s2104/mirimunse/LuckRecordActivity.java
@@ -2,13 +2,28 @@ package kr.hs.s2104.mirimunse;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LuckRecordActivity extends AppCompatActivity {
+    TextView textV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_luck_record);
+        textV = findViewById(R.id.textv);
+        textV.setOnClickListener(textViewListenerListener);
     }
+
+    View.OnClickListener textViewListenerListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(getApplicationContext(), LuckRecordDetailActivity.class);
+            startActivity(intent);
+        }
+    };
+
 }
