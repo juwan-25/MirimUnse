563f7c957c50a867dc0e215e7a7ee186533a9c8a
steam88ys <yssteam88@gmail.com>
2022-08-04 08:56:16 +0900

    설정 화면 임시 완성

--- app/src/main/java/kr/hs/s2104/mirimunse/SettingsActivity.java
+++ app/src/main/java/kr/hs/s2104/mirimunse/SettingsActivity.java
@@ -5,16 +5,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class SettingsActivity extends AppCompatActivity {
     ImageView checkMain;
     TextView recordMain, toolMain;
+    String[] detail1 = {"개발툴", "Android Studio Java"};
+    String[] detail2 = {"개발 참여자", "2102 김윤서", "2104 백지민", "2211이주완"};
+    String[] detail3 = {"개발기간", "2022.07.25~"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        Spinner spinner1 = findViewById(R.id.spinner1);
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, detail1);
+        spinner1.setAdapter(adapter1);
+
+        Spinner spinner2 = findViewById(R.id.spinner2);
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, detail2);
+        spinner2.setAdapter(adapter2);
+
+
+        Spinner spinner3 = findViewById(R.id.spinner3);
+        ArrayAdapter<String> adapter3 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, detail3);
+        spinner3.setAdapter(adapter3);
 
         toolMain = findViewById(R.id.text_tool);
         toolMain.setOnClickListener(new View.OnClickListener() {
