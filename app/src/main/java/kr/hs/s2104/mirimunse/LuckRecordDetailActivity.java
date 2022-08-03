1c88eb191d82ffd241a0e5fafdf00d472d979346
steam88ys <yssteam88@gmail.com>
2022-08-03 19:43:12 +0900

    버튼 최소 사이즈 수정

--- app/src/main/java/kr/hs/s2104/mirimunse/LuckRecordDetailActivity.java
+++ app/src/main/java/kr/hs/s2104/mirimunse/LuckRecordDetailActivity.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class LuckRecordDetailActivity extends AppCompatActivity {
