4c3c78521ed970ec3a516efb9bfc547613d8eb5f
steam88ys <yssteam88@gmail.com>
2022-08-05 09:29:40 +0900

    listView로 임시 수정

--- app/src/main/java/kr/hs/s2104/mirimunse/LuckRecordActivity.java
+++ app/src/main/java/kr/hs/s2104/mirimunse/LuckRecordActivity.java
@@ -5,19 +5,40 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LuckRecordActivity extends AppCompatActivity {
-    TextView textV, toolMain;
+    TextView toolMain;
     ImageView checkMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_luck_record);
-        textV = findViewById(R.id.textv);
-        textV.setOnClickListener(textViewListenerListener);
+
+        ListView listView = findViewById(R.id.listv);
+
+        List<String> list = new ArrayList<>();
+        list.add("22.08.05 OO이의 운세");
+        list.add("22.08.04 OO이의 운세");
+        list.add("22.08.02 OO이의 운세");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String >(this, android.R.layout.simple_list_item_1, list);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                String data = (String) adapterView.getItemAtPosition(position);
+            }
+        });
 
         toolMain = findViewById(R.id.text_tool);
         toolMain.setOnClickListener(new View.OnClickListener() {
@@ -38,13 +59,5 @@ public class LuckRecordActivity extends AppCompatActivity {
         });
 
     }
-
-    View.OnClickListener textViewListenerListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            Intent intent = new Intent(getApplicationContext(), LuckRecordDetailActivity.class);
-            startActivity(intent);
-        }
-    };
 
 }
