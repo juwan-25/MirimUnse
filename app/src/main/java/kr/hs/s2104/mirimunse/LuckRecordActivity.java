a9d8520acd6ab91739319326f7dc47a0b09c2607
steam88ys <yssteam88@gmail.com>
2022-08-19 11:36:21 +0900

    activity_luck_record.xml 왼쪽 선 이미지 추가

--- app/src/main/java/kr/hs/s2104/mirimunse/LuckRecordActivity.java
+++ app/src/main/java/kr/hs/s2104/mirimunse/LuckRecordActivity.java
@@ -22,7 +22,6 @@ public class LuckRecordActivity extends AppCompatActivity {
     TextView toolMain, unseRecord;
     ImageView checkMain;
     LinearLayout list;
-
     DatabaseHelper dbHelper;
     SQLiteDatabase db;
 
@@ -60,24 +59,17 @@ public class LuckRecordActivity extends AppCompatActivity {
             view1.setTextSize(FONT_SIZE);
             view1.setCompoundDrawablesWithIntrinsicBounds( R.drawable.dot, 0, R.drawable.threedot, 0);  // 양쪽 버튼 이미지
             view1.setTextColor(Color.rgb(251,218,218)); //글자색상 rgb로 코드 변환
+
             //layout_width, layout_height, gravity 설정
             LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-
             lp.gravity = Gravity.CENTER;
+            lp.bottomMargin = 40;
             view1.setLayoutParams(lp);
+
             //부모 뷰에 추가
             container.addView(view1);
 
         }
-
-        /*TextView view_img = new TextView(this);
-        view_img.setCompoundDrawablesWithIntrinsicBounds( 0, 0, 0, R.drawable.mainunbtn_img);  // 하단 버튼 이미지
-        //layout_width, layout_height, gravity 설정
-        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
-        lp.gravity = Gravity.BOTTOM;
-        view_img.setLayoutParams(lp);
-        //부모 뷰에 추가
-        container.addView(view_img);*/
 
         unseRecord.setOnClickListener(new View.OnClickListener() {    // luck_record_detail 연결
             @Override
