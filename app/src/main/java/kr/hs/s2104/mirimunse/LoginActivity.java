4c5af77521898b92d3ac84a946e88de239490ad1
juwan-25 <s2109@e-mirim.hs.kr>
2022-09-27 19:49:48 +0900

    로그인 회원가입 xml 완성

---
+++ app/src/main/java/kr/hs/s2104/mirimunse/LoginActivity.java
@@ -0,0 +1,14 @@
+package kr.hs.s2104.mirimunse;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/kr/hs/s2104/mirimunse/SignUpActivity.java
@@ -0,0 +1,13 @@
+package kr.hs.s2104.mirimunse;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
