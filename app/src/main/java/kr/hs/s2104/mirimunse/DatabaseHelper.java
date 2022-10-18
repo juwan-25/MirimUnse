85a3b967fdbc2691bda0c98c31729c1fba37ad41
지민 백 <qorwlalsjimin@naver.com>
2022-10-18 20:00:57 +0900

    assets 폴더에서 DB 복사

--- app/src/main/java/kr/hs/s2104/mirimunse/DatabaseHelper.java
+++ app/src/main/java/kr/hs/s2104/mirimunse/DatabaseHelper.java
@@ -9,12 +9,66 @@ import android.util.Log;
 
 import androidx.annotation.Nullable;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static String DB_PATH = "";
+    private static String DB_NAME = "MirimUnsae.db";
+    private Context mContext;
 
     //DB 생성
     public DatabaseHelper(Context context) {
-        super(context, "MirimUnsae.db", null, 1);
-        Log.d(this.getClass().getName() ,"***DatabaseHelper 생성자 실행");
+        super(context, DB_NAME, null, 1);
+
+        DB_PATH = "/data/data/" + context.getPackageName() + "/databases/";
+        this.mContext = context;
+        dataBaseCheck();
+    }
+
+    private void dataBaseCheck()
+    {
+        File dbFile = new File(DB_PATH + DB_NAME);
+
+        if (!dbFile.exists())
+        {
+            dbCopy();
+        }
+    }
+
+    // db를 assets에서 복사해온다.
+    private void dbCopy()
+    {
+        try
+        {
+            File folder = new File(DB_PATH);
+            if (!folder.exists())
+            {
+                folder.mkdir();
+            }
+
+            InputStream inputStream = mContext.getAssets().open(DB_NAME);
+            String out_filename = DB_PATH + DB_NAME;
+            OutputStream outputStream = new FileOutputStream(out_filename);
+            byte[] mBuffer = new byte[1024];
+            int mLength;
+            while ((mLength = inputStream.read(mBuffer)) > 0)
+            {
+                outputStream.write(mBuffer, 0, mLength);
+            }
+            outputStream.flush();
+            ;
+            outputStream.close();
+            inputStream.close();
+
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
     }
 
     @Override
